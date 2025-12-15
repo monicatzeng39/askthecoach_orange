88957d197176ac094ce4afcfdcb520b92ba7944b
monicatzeng39 <monica.tzeng39@gmail.com>
2025-12-15 17:55:00 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
    return {
   base: '/askthecoach_orange/',
-  server: { ... },
-  plugins: [react()],
+  server: {
   port: 3000,
   host: '0.0.0.0',
       },
